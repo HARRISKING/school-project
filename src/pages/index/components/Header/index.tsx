fbf090edfd2d15cd95d83d0584969da3c2117449
suixing <suixing@uni-ubi.com>
2022-04-27 19:46:43 +0800

    feat: 首页完成

--- src/pages/index/components/Header/index.tsx
+++ src/pages/index/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import dot from '@/assets/images/dot.svg';
+
 import styles from './index.less';
 
 interface IHeaderProps {
@@ -9,26 +11,41 @@ const Header: React.FC<IHeaderProps> = ({ dataSource }) => {
   const { background_color, title_list, content, background_image } =
     dataSource?.top_screen;
   return (
-    <div className={styles.headerBox} style={{ background: background_color }}>
-      <div className={styles.titleBox}>
-        {(title_list || []).map((item: string) => (
-          <div className={styles.headerTitle}>{item}</div>
-        ))}
-      </div>
-      <ul className={styles.directory}>
-        {(dataSource?.module_List || []).map((item: any) => (
-          <li>
-            <a href={`#${item.key}`} className={styles.directoryItem}>
-              {item?.title}
-            </a>
-          </li>
-        ))}
-        <img className={styles.backgroundImage} src={background_image} alt="" />
-      </ul>
-      <div className={styles.contentsBox}>
-        <div className={styles.verifyText}>
-          ABOUT<span style={{ color: '#f24034' }}>US</span>
+    <div className={styles.headerBox}>
+      <div
+        className={styles.bannerBox}
+        style={{ background: background_color }}
+      >
+        <div className={styles.titleBox}>
+          {(title_list || []).map((item: string) => (
+            <div className={styles.headerTitle}>{item}</div>
+          ))}
         </div>
+        <ul className={styles.directory}>
+          {(dataSource?.module_List || []).map((item: any) => (
+            <li>
+              <a href={`#${item.key}`} className={styles.directoryItem}>
+                {item?.title}
+              </a>
+            </li>
+          ))}
+          <img
+            className={styles.backgroundImage}
+            src={background_image}
+            alt=""
+          />
+        </ul>
+      </div>
+      <div className={styles.contentsBox}>
+        <div className={styles.leftBox}>
+          <div className={styles.verifyText}>
+            ABOUT<span style={{ color: '#f24034' }}>US</span>
+            <div className={styles.line} />
+          </div>
+          <div className={styles.dot} />
+        </div>
+
+        <div className={styles.content}>{content}</div>
       </div>
     </div>
   );
